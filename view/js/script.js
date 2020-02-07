a3ce3df26044dcbcf9fa47392a554ca9302efee8
Jairo Bernal <jairo.bernal@send.sd>
2020-02-06 21:57:35 -0500

    ad jquery

--- view/js/script.js
+++ view/js/script.js
@@ -1,1 +1,32 @@
-const headers = ['SL', 'Date', 'Diagnosis', 'Weight', 'Doctor'];
+const headers = ['SL', 'Date', 'Diagnosis', 'Weight', 'Doctor']
+
+function displayPatient(response){
+    console.log(response)
+}
+
+
+$(document).ready(() => {
+  $('#fetch-records').submit(e => {
+    e.preventDefault()
+    let patient = $('#patient-select')
+    let loader = $('#loader-view')
+    if(patient.val() !== null){
+        loader.show()
+        $.ajax({
+            type:'GET',
+            url: 'https://jsonmock.hackerrank.com/api/medical_records',
+            data: {
+                userId: patient.val()
+            },
+            success : (response) =>{
+                displayPatient(response)
+                loader.hide()
+            },
+            error : (err) => {
+                loader.hide()
+            }
+        })
+    }
+
+  })
+})
